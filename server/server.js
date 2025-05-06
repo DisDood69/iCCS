9823936bef8d209f86427b42bd2b6ac40abd5fd5
DisDood69 <guilalas.patricklenard@gmail.com>
2025-05-07 00:43:21 +0800

    iCCS initial commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const path = require("path");
+
+const port = 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user:  "root",
+    password: "",
+    database: "iccs"
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
